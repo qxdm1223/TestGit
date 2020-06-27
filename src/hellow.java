c1a4998b1d68446cec445f0a051a33bd0e786baf
qxdm1223 <1872065148@qq.com>
2020-06-27 23:18:56 +0800

    3

--- src/hellow.java
+++ src/hellow.java
@@ -2,6 +2,6 @@ public class hellow {
     public static void main(String[] args) {
         System.out.println("hellow");
         //System.out.println("nihao");
-        System.out.println("saliya");
+        System.out.println("tainanle");
     }
 }
